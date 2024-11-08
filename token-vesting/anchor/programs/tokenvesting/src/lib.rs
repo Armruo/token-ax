a1cbdbd7cf1e6c110cdf1b26bedc99192b0b7452
Armruo <armruo@outlook.com>
2024-11-08 17:28:00 +0800

    account&instruction: create_employee_account

--- token-vesting/anchor/programs/tokenvesting/src/lib.rs
+++ token-vesting/anchor/programs/tokenvesting/src/lib.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{ self, Mint }
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{ self, Mint, TokenAccount, TokenInterface, TransferChecked };
 
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
@@ -21,6 +22,31 @@ pub mod tokenvesting {
       };
 
       Ok(()) 
+    }
+
+    pub fn create_employee_account(
+      ctx: Context<CreateEmployeeAccount>,
+      start_time: i64,
+      end_time: i64,
+      total_amount: i64,
+      cliff_time: i64
+    ) -> Result<()> {
+      *ctx.accounts.employee_account = EmployeeAccount {
+        beneficiary: ctx.accounts.beneficiary.key(),
+        start_time,
+        end_time,
+        total_amount,
+        total_withdrawn: 0,
+        cliff_time,
+        vesting_account: ctx.accounts.vesting_account.key(),
+        bump: ctx.bumps.employee_account,
+      };
+
+      Ok(())
+    }
+
+    pub fn claim_tokens(ctx: Context<ClaimTokens>) -> Result<()> {
+      Ok(())
     }
 
     
@@ -63,6 +89,38 @@ pub struct VestingAccount {
   #[max_len(50)]
   pub company_name: String,
   pub treasury_bump: u8,
+  pub bump: u8,
+}
+
+
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+  #[account(mut)]
+  pub owner: Signer<'info>,
+  pub beneficiary: SystemAccount<'info>,
+  #[account(has_one = owner)]
+  pub vesting_account: Account<'info, VestingAccount>,
+  #[account(
+    init,
+    space = 8 + EmployeeAccount::INIT_SPACE,
+    payer = owner,
+    seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+    bump
+  )]
+  pub employee_account: Account<'info, EmployeeAccount>,
+  pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+  pub beneficiary: Pubkey,
+  pub start_time: i64,
+  pub end_time: i64,
+  pub total_amount: i64,
+  pub total_withdrawn: i64,
+  pub cliff_time: i64,
+  pub vesting_account: Pubkey,
   pub bump: u8,
 }
 
